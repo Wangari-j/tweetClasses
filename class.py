361805f5bcc1f7d1bd06ae2bc3ed76f35d1ee697
Wangari-j <wangarijudy@gmail.com>
2024-03-06 23:24:06 +0300

    classes

---
+++ class.py
@@ -0,0 +1,12 @@
+class Electronnic:
+    def __init__(self):
+        print("1")
+
+class OSDevice:
+    def __init__(self):
+        print("2")
+
+Electronnic = E1
+OSDevice = 0SD
+
+Electronnic.E1()
